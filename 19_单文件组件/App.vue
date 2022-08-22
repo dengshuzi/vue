f20272c54e85c8db3561f72e2f3406e26c47f558
dengshuzi <408664164@qq.com>
2022-08-22 19:56:27 +0800

    19

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+  //引入组件
+  import {School, Student} from './'
+import Student from './Student.vue';
+
+  export default {
+    name: 'App',
+    components: {
+    School,
+    Student,
+    Student
+}
+  }
+</script>
