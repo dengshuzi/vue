4dbd2e1d29ac96fbaa9b8f04230b530c78ae2237
dengshuzi <408664164@qq.com>
2022-09-02 22:49:08 +0800

    vue_test 23

--- vue_test/23_src_求和案例_纯vue版/main.js
+++ vue_test/23_src_求和案例_纯vue版/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import mitt from 'mitt'
-
 //引入插件
 import vueResource from 'vue-resource'
 const app = createApp(App)
 app.config.globalProperties.$bus = new mitt();
 // app.use(vueResource)
-app.use(Vuex).mount('#app')
+app.mount('#app')
