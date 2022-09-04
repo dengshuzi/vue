5b2ae23528f825e026ac5cb246e483eb5212b721
dengshuzi <408664164@qq.com>
2022-09-04 21:05:31 +0800

    vue_test 25 26 27

--- vue_test/src/components/Count.vue
+++ vue_test/src/components/Count.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
-    <h1>当前求和为: {{$store.state.sum}}</h1>
-    <h1>当前求和放大10倍为: {{$store.getters.bigSum}}</h1>
+    <h1>当前求和为: {{sum}}</h1>
+    <h1>当前求和放大10倍为: {{bigSum}}</h1>
+    <h3>我在{{school}}, 学习{{subject}}</h3>
+    <h3 style="color:red">Person组件的总人数是: {{personList.length}}</h3>
     <select v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
     </select>
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
-    <button @click="incrementOdd">当前求和为基数再加</button>
-    <button @click="incrementWait">等一等再加</button>
+    <button @click="increment(n)">+</button>
+    <button @click="decrement(n)">-</button>
+    <!-- <button @click="JIA(n)">+</button>
+    <button @click="JIAN(n)">-</button> -->
+    <button @click="incrementOdd(n)">当前求和为基数再加</button>
+    <button @click="incrementWait(n)">等一等再加</button>
+    <!-- <button @click="jiaOdd(n)">当前求和为基数再加</button>
+    <button @click="jiaWait(n)">等一等再加</button> -->
   </div>
 </template>
 
 <script>
+  import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
   export default {
     name: 'Count',
     data() {
@@ -23,18 +30,21 @@
       }
     },
     methods: {
-      increment () {
-        this.$store.commit('JIA', this.n);
-      },
-      decrement () {
-        this.$store.commit('JIAN', this.n);
-      },
-      incrementOdd () {
-        this.$store.dispatch('jiaOdd', this.n);
-      },
-      incrementWait () {
-        this.$store.dispatch('jiaWait', this.n);
-      }
+      //借助mapMutations生成对应的方法, 方法中会调用commit去联系mutations(对象写法)
+      ...mapMutations('countAbout', {increment: 'JIA', decrement: 'JIAN'}),
+
+      //借助mapActions, 方法中会调用dispatch去联系actions(数组写法)
+      ...mapActions('countAbout', {incrementOdd: 'jiaOdd', incrementWait: 'jiaWait'}),
+    },
+    computed: {
+      //借助mapState生成计算属性, 从state中读取数据(数组写法)
+      ...mapState('countAbout', ['sum', 'school', 'subject']),
+      ...mapState('personAbout', ['personList']),
+
+      //借助mapGetters生成计算属性, 从state中读取数据(数组写法)
+      ...mapGetters('countAbout', ['bigSum'])
+    },
+    mounted() {
     }
   }
 </script>
