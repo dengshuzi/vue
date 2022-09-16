bc687b24f3db934e5ec7e773b11ae7de2b529c06
dengshuzi <408664164@qq.com>
2022-09-16 21:07:13 +0800

    vue3_test 02 03

---
+++ vue3_test/03_src_ref函数/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>一个人的信息</h1>
+    <h2>姓名: {{name}}</h2>
+    <h2>年龄: {{age}}</h2>
+    <h3>工作种类: {{job.type}}</h3>
+    <h3>工作薪资: {{job.salary}}</h3>
+    <button @click="changeInfo">修改个人信息</button>
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  export default {
+    name: 'App',
+    setup() {
+      // 数据
+      let name = ref('张三')
+      let age = ref(18)
+      let job = ref({
+        type: 'frontend',
+        salary: '30k'
+      })
+
+      // 方法
+      function changeInfo() {
+        name.value = '李四'
+        age.value = 48 
+        console.log(job);
+        job.value.type = 'backend'
+        job.value.salary = '40k'
+      }
+
+      // 返回一个对象(常用)
+      return {
+        name,
+        age,
+        changeInfo,
+        job
+      }
+    }
+  }
+</script>
--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,85 +1,52 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h1>一个人的信息</h1>
+    <h2>姓名: {{person.name}}</h2>
+    <h2>年龄: {{person.age}}</h2>
+    <h3>工作种类: {{person.job.type}}</h3>
+    <h3>工作薪资: {{person.job.salary}}</h3>
+    <h3>爱好: {{person.hobby}}</h3>
+    <h3>测试的数据c: {{person.job.a.b.c}}</h3>
+    <button @click="changeInfo">修改个人信息</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+  import { reactive } from 'vue'
+  export default {
+    name: 'App',
+    setup() {
+      // 数据
+      let person = reactive({
+        name: '张三',
+        age:18,
+        job: {
+          type: 'frontend',
+          salary: '30k',
+          a: {
+            b: {
+              c: 666
+            }
+          }
+        },
+        hobby: ['抽烟', '喝酒', '烫头']
+      })
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+      // 方法
+      function changeInfo() {
+        person.name = '李四'
+        person.age = 48
+        person.job.type = 'backend'
+        person.job.salary = '40k'
+        person.job.a.b.c = 999
+        person.hobby[0] = '学习'
+      }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+      // 返回一个对象(常用)
+      return {
+        person,
+        changeInfo
+      }
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
